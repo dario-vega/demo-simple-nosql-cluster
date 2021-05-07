5455468083edae6705648d96e045ba1b904eddfe
dario-vega <66302636+dario-vega@users.noreply.github.com>
2021-05-07 12:10:11 +0200

    Update env.sh

--- script/env.sh
+++ script/env.sh
@@ -11,4 +11,4 @@ echo $KVHOST
 alias kv_sql="java -jar $KVHOME/lib/sql.jar -helper-hosts node1-nosql:5000 -store OUG "
 alias kv_admin="java -jar $KVHOME/lib/kvstore.jar runadmin -port 5000 -host node1-nosql"
 alias kv_ping="java -jar $KVHOME/lib/kvstore.jar ping  -port 5000 -host node1-nosql"
-alias kv_proxy="java -jar $KVHOME/lib/httpproxy.jar -helperHosts node1-nosql:5000 -storeName OUG -httpPort 8080 -verbose true"
+alias kv_proxy="java -jar $KVHOME/lib/httpproxy.jar -helperHosts node1-nosql:5000 -storeName OUG -httpPort 80 -verbose true"
