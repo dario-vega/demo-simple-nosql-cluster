6abed205563660cc2a67591174d2563449d16695
dario-vega <66302636+dario-vega@users.noreply.github.com>
2021-06-24 17:11:49 +0200

    Update mrtable-agent-stat.sh

--- script/mrtable-agent-stat.sh
+++ script/mrtable-agent-stat.sh
@@ -1,4 +1,4 @@
 echo "connect store -name $KVSTORE -host localhost -port 5000" > mrtable-agent.kvs
 echo "show mrtable-agent-statistics -agent 0 -json" >> mrtable-agent.kvs
-java -jar $KVHOME/lib/kvstore.jar runadmin -port 5000 -host localhost  load -file  mrtable-agent.kvs | grep -v Connected | jq '. "returnValue"."0"."statistics"."regionStat"."FR"."laggingMs"."max"'
+java -jar $KVHOME/lib/kvstore.jar runadmin -port 5000 -host localhost  load -file  mrtable-agent.kvs | grep -v Connected | jq '. "returnValue"[]."statistics"."regionStat"[]."laggingMs"."max"'
 
