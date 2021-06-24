48aa1e5a6448b27a916281e06963b5c8a9809220
dario-vega <66302636+dario-vega@users.noreply.github.com>
2021-06-24 16:03:22 +0200

    Update clean.sh
    adding support for KVXRS

--- script/clean.sh
+++ script/clean.sh
@@ -6,12 +6,13 @@ mkdir -p ${KVDATA}/disk1
 mkdir -p ${KVDATA}/disk2
 mkdir -p ${KVDATA}/disk3
 mkdir -p /home/opc/kvstore_export
-
+mkdir -p $KVXRS
 
 ls -lrtd $KVROOT
 ls -lrt  $KVROOT
 ls -lRtd $KVDATA
 ls -lRt  $KVDATA
+ls -lRt $KVXRS
 
 java -Xmx64m -Xms64m -jar $KVHOME/lib/kvclient.jar
 
